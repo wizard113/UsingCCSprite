2c929c25211f4551e1d112d28d4f3ba0a5407083
Youngrok Lee <yeong.rok.lee@gmail.com>
2012-03-02 16:13:50 +0900

    Setup Project Start!!

---
+++ UsingCCSprite/MainLayer.h
@@ -0,0 +1,16 @@
+//
+//  MainLayer.h
+//  UsingCCSprite
+//
+//  Created by 이영록 on 12. 3. 2..
+//  Copyright 2012년 ildo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface MainLayer : CCLayer {
+    
+}
++(id)scene;
+@end
---
+++ UsingCCSprite/MainLayer.m
@@ -0,0 +1,33 @@
+//
+//  MainLayer.m
+//  UsingCCSprite
+//
+//  Created by 이영록 on 12. 3. 2..
+//  Copyright 2012년 ildo. All rights reserved.
+//
+
+#import "MainLayer.h"
+
+
+@implementation MainLayer
++(CCScene *) scene
+{
+	CCScene *scene = [CCScene node];
+	MainLayer *layer = [MainLayer node];
+	[scene addChild: layer];
+	return scene;
+}
+
+-(id) init
+{
+	if( (self=[super init])) {
+		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
+		CGSize size = [[CCDirector sharedDirector] winSize];
+		label.position =  ccp( size.width /2 , size.height/2 );
+		[self addChild: label];
+	}
+	return self;
+}
+
+
+@end
